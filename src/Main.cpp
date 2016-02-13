70cd07d69746cfe4de6b3ecf0b912ff5913950db
Nam Nguyen <nnguy072@ucr.edu>
2016-02-12 22:43:44 -0800

    removed all testing comments in Main.cpp and added login/hostname, renamed makefile to Makefile

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,18 +1,21 @@
 #include "Shell_Base.h"
 #include "Parser.h"
 #include "Executer.h"
-//#include <unistd.h>
+#include <unistd.h>
 using namespace std;
 
 int main()
 {
-	//string* loginName = getlogin();
+	char hostName[300];
+	char loginName[300];
+	getlogin_r(loginName, sizeof(loginName));
+	gethostname(hostName, sizeof(hostName));
 	string cmd;
 	int status;
 
 	do
 	{
-		cout << "$ ";
+		cout << loginName << "@" << hostName << "$ ";
 		getline(cin, cmd);
 	}while(cmd.empty() || (cmd.find("#") == 0));
 
@@ -30,41 +33,9 @@ int main()
 		if (status == -1)
 			return -1;
 
-		cout << "$ ";
+		cout << loginName << "@" << hostName << "$ ";
 		getline(cin, cmd);
 	}
-
-	// testing list of arguments
-	// TODO: learn how to fix this error with these things vvv
-	//char *args1[] = {"/bin/", (char*) 0};	// this is just "ls" by itself
-	//char *args[] = {"/bin/ls", "-a", (char *) 0};	// this is "ls -a"
-
-	// TODO: fix exit bug where when an incorrect command causes exit not to work properly afterwards.
-	
-	// EX: "l" is not a command, type in string exit and does nothing.  SUPPOSE to exit.  After typing
-	// string exit the second time shell exits.
-	// I'm speculating it has to do with how waitpid works.
-	
-	// EX2: "l" is not a command, type in ls and ls not a working directory. SUPPOSE to list. After
-	// typing string ls the second time the shell lists
-	
-	// Ex3: "l" is not a command, type in ls -a and it works. EXPECTED the need to type it again in order
-	// for it to take effect.
-	
-	//Also notice if you try too much fork will fail and execvp will fail with EX2.  And notice that
-	//the message "ls cannot access __".  The underscore changes letters depending on previous input
-	//For instance it can be ls cannot access a or ls cannot access ls or some random.
-
-	//echo can't echo out more than one word
-	
-	//specs require to use perror and a directory called tests taht should contain bash scripts that test
-	//cases
-
-
-	//ADDED: added a exit functionality but cannot complete with unknown error checking bugs and connector.
-	//Added proper destructor in Execute.cpp
-	//Added proper destructor in Shell_Base.cpp and was called in main as after b->execute
-
 
 	return 0;
 }
